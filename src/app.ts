21e5769e7448e35dceb4dd1e46ce7e2ad449dc1d
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-02-14 16:07:57 +0600

    router,controller, service

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/api/v1/students', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
