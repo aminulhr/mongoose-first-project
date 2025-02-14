63cf3ef52f3d977ef5b7831202ec0ade9377e136
Aminul Haque Rana <aminulhaquerana1@gmail.com>
2025-02-14 11:46:08 +0600

    projest setup updata

--- src/app.ts
+++ src/app.ts
@@ -7,11 +7,6 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  const a = 19;
-  const b = 19;
-  const df = 'dfdsa';
-  const d = 'fkdfksd';
-
   res.send('Hello World!');
 });
 
